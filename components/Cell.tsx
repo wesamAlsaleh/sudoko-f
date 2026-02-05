2f557349bb04fe777fb8449b5378952c7896b00e
wesamAlsaleh <wesammuneer7@gmail.com>
2026-02-06 02:58:05 +0300

    feat: highlight the selected cell and row and column

--- components/Cell.tsx
+++ components/Cell.tsx
@@ -1,30 +1,42 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function Cell({
   number,
   cellId,
+  selectedIndex,
+  setSelectedIndex,
 }: {
   number: string;
   cellId: number;
+  selectedIndex: number | null;
+  setSelectedIndex: () => void;
 }) {
   // get the cell coordinates
-  const row = Math.floor(cellId / 9); // get the row index (0 -> 8)
+  const row = Math.floor(cellId / 9); // get the row index (0 -> 8), Math.floor(index / 9) converts a linear 0-80 index into a 0-8 row number
   const column = cellId % 9; // get the column index (0 -> 8)
 
   // custom border style based on the location (after each 3 cells)
   const rowStyle = row % 3 == 0 && row != 0 ? "border-t-4" : ""; // add the divider in the rows that are dividable by 3 and not the first row
   const columnStyle = column % 3 == 0 && column != 0 ? "border-l-4" : ""; // add the divider in the columns that are dividable by 3 and not the first column
 
+  // cell highligh logic
+  const isSelected = selectedIndex === cellId; // true if this specific cell is the one the user clicked
+  const isSameRow =
+    selectedIndex != null && row === Math.floor(selectedIndex / 9); // true if this cell shares the same horizontal row as the selection
+  const isSameColumn = selectedIndex != null && column === selectedIndex % 9; // true if this cell shares the same vertical column as the selection
+
   return (
     <div
-      className={`w-12 h-12 border border-zinc-200 flex items-center justify-center text-zinc-800 text-lg font-semibold hover:bg-gray-200 hover:cursor-pointer ${rowStyle} ${columnStyle}`}
+      className={`w-12 h-12 flex items-center justify-center border border-zinc-200 text-zinc-800 text-lg font-semibold cursor-pointer select-none ${rowStyle} ${columnStyle}  ${isSelected ? "bg-blue-300" : ""} ${!isSelected && (isSameRow || isSameColumn) ? "bg-blue-100" : ""}`}
       onClick={() => {
-        console.log(number);
+        // update state for the UI
+        setSelectedIndex();
       }}
     >
       {number === "0" ? "" : number}
+      {/* {`${row}${column}`} */}
     </div>
   );
 }
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Cell from "./Cell";
 
 export default function Grid({ puzzleString }: { puzzleString: string }) {
+  // state to manage the cell color
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+
   // convert the puzzle string (81-char) into an array
   const cellNumbers = puzzleString.split(""); // ["1", "2", ..., "81"]
 
@@ -13,7 +18,15 @@ export default function Grid({ puzzleString }: { puzzleString: string }) {
       {/* grid contents container */}
       <div className="grid grid-cols-9 border-2 border-zinc-900 w-fit">
         {cellNumbers.map((cellNumber, index) => {
-          return <Cell key={index} number={cellNumber} cellId={index} />;
+          return (
+            <Cell
+              key={index} // unique ID
+              number={cellNumber} // the puzzle number
+              cellId={index} //the cell index
+              selectedIndex={selectedIndex} // current selected index
+              setSelectedIndex={() => setSelectedIndex(index)} // on click set the selected index
+            />
+          );
         })}
       </div>
     </div>
