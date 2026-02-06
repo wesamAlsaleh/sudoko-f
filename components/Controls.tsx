0fd84fe28d8897698427355bda55f11f4b248ce1
wesamAlsaleh <wesammuneer7@gmail.com>
2026-02-06 22:37:34 +0300

    feat: add controls buttons

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Button } from "./ui/button";
+import { Lightbulb, RotateCcw } from "lucide-react";
 
 export default function Controls() {
-  return <div>Controls</div>;
+  return (
+    <div className="flex gap-2 items-center justify-center mt-4">
+      {/* New game button */}
+      <Button size="icon-lg" className="w-40">
+        <RotateCcw />
+        <span className="font-semibold">New Game</span>
+      </Button>
+
+      {/* Hint button */}
+      <Button size="icon-lg" className="w-20">
+        <Lightbulb />
+        <span className="font-semibold">Hint</span>
+      </Button>
+    </div>
+  );
 }
