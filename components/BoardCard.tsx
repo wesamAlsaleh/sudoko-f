48c6425260885f2fcf87318605a57e3f44b1496e
wesamAlsaleh <wesammuneer7@gmail.com>
2026-02-07 17:05:21 +0300

    feat: add fetching logic bases on the uuid in the url

--- components/BoardCard.tsx
+++ components/BoardCard.tsx
@@ -24,45 +24,81 @@ export default function BoardCard({ uuid }: { uuid: string | undefined }) {
 
   // fetch the game
   useEffect(() => {
-    // function to fetch a new game
-    const fetchGame = async () => {
+    // function to initialize a game
+    const initializeGame = async () => {
+      // update UI state
+      setLoading(true);
+
+      // get the game data from the localstorage
+      const localUUID = localStorage.getItem("gid");
+      const localPuzzleString = localStorage.getItem("ps");
+
+      // if the url contain an id, fetch from the server (e.g., someone shared a link)
+      if (uuid) {
+        // if the uuid in the url is different from the one in the storage OR there is no game in the storage fetch the new game
+        if (uuid !== localUUID || !localUUID) {
+          await fetchGameFromServer(uuid); // only fetch if it's different from what we already have in state
+        }
+        // else set the game from the localstorage
+        else {
+          // set the puzzle state
+          setPuzzleString(localPuzzleString);
+          // update UI state
+          setLoading(false);
+        }
+        return;
+      }
+
+      // if there is no url, but localstorage has a saved game, load it from the storage
+      if (localUUID && localPuzzleString) {
+        // set the puzzle state
+        setPuzzleString(localPuzzleString);
+
+        // update the URL with the id in the localstorage
+        router.replace(`?id=${localUUID}`, { scroll: false });
+
+        // update UI state
+        setLoading(false);
+        return;
+      }
+
+      // default if there is no URL, no localstorage, stay empty until the player clicks new game
+      setPuzzleString("0".repeat(81)); // set the puzzle state
+      setLoading(false); // update UI state
+    };
+
+    // function to fetch a game from the server (generate new or load a game)
+    const fetchGameFromServer = async (uuid?: string) => {
       try {
-        setLoading(true);
         //if the uuid exists load the game, otherwise generate a new puzzles
         const data = uuid
           ? await gameService.getGame(uuid)
           : await gameService.createNewGame();
 
-        // update the URL to include the new gameId
-        if (data.gameId) {
-          router.replace(`?id=${data.gameId}`, { scroll: false });
-        }
-
-        // set the puzzle
+        // set the puzzle state
         setPuzzleString(data.puzzleString);
 
-        // add the puzzle in the localstorage
+        // update URL
+        router.replace(`?id=${data.gameId}`, { scroll: false });
+
+        // update LocalStorage
         localStorage.setItem("gid", data.gameId);
         localStorage.setItem("ps", data.puzzleString);
         localStorage.setItem("d", data.difficulty);
       } catch (error) {
-        console.error("Error fetching Sudoku:", error);
+        // log the error
+        console.error("Fetch failed:", error);
+
+        // make the puzzle empty
+        setPuzzleString("0".repeat(81));
       } finally {
         setLoading(false);
       }
     };
 
-    // TODO: check the localstorage if there is a generated game, if not generate a new game
-    const localUUID = localStorage.getItem("gid");
-    const localPuzzleString = localStorage.getItem("ps");
-
-    if (localUUID) {
-    }
-    // fetch the new game
-    fetchGame();
-  }, [uuid]);
-
-  console.log(puzzleString);
+    // initialize a game
+    initializeGame();
+  }, [uuid]); // listen to url changes
 
   return (
     // Card container
