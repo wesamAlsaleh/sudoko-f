a40061ec1ca6d252da62ad5a5dfaef7991657d7f
wesamAlsaleh <wesammuneer7@gmail.com>
2026-02-07 17:26:15 +0300

    feat: add requesting new game from the backend functionality

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,12 +1,44 @@
+"use client";
+
 import React from "react";
 import { Button } from "./ui/button";
 import { Lightbulb, RotateCcw } from "lucide-react";
+import { gameService } from "@/service/gameService";
+import { useRouter } from "next/navigation";
 
 export default function Controls() {
+  // router instance
+  const router = useRouter();
+
+  // function to handle generating new game
+  const handleGeneratingNewGame = async () => {
+    try {
+      // request new game from the server
+      const data = await gameService.createNewGame();
+
+      // update URL
+      router.replace(`?id=${data.gameId}`, { scroll: false });
+
+      // update LocalStorage
+      localStorage.setItem("gid", data.gameId);
+      localStorage.setItem("ps", data.puzzleString);
+      localStorage.setItem("d", data.difficulty);
+
+      // force a full page reload to the new URL
+      window.location.href = `/?id=${data.gameId}`;
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex gap-2 items-center justify-center mt-4">
       {/* New game button */}
-      <Button size="icon-lg" className="w-40">
+      <Button
+        size="icon-lg"
+        className="w-40"
+        onClick={() => handleGeneratingNewGame()}
+      >
         <RotateCcw />
         <span className="font-semibold">New Game</span>
       </Button>
